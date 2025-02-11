a03eab13fa1dad9bb4533e92d78815014703f124
frc7694 <frc7694@fake.is-a.dev>
2025-02-10 21:43:17 -0500

    Add current robot identification

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,15 +10,14 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Constants {
   public static final Mode simMode = Mode.SIM;
   public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
+  public static final Robots currentRobot = BuildConstants.DEPLOY_SSID.contains("FRC") ? Robots.Ferry : Robots.Terry;
 
-  public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
-
-    /** Running a physics simulator. */
-    SIM,
-
-    /** Replaying from a log file. */
-    REPLAY
+  public enum Mode {
+    REAL, SIM, REPLAY
   }
+
+  public enum Robots {
+    Terry, Ferry;
+  }
+
 }
