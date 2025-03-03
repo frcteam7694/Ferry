294e613e6608f459387f97b2c891b51c2f8d78e0
frc7694 <frc7694@fake.is-a.dev>
2025-03-03 13:45:18 -0500

    Fix RotationUtil to loop 360 back to 0

--- src/main/java/frc/robot/util/RotationUtil.java
+++ src/main/java/frc/robot/util/RotationUtil.java
@@ -9,10 +9,15 @@ public class RotationUtil {
   }
 
   public static Rotation2d diff(Rotation2d a, Rotation2d b) {
-    return abs(a.rotateBy(b.unaryMinus()));
+    return normalize(abs(a.rotateBy(b.unaryMinus())));
   }
 
   public static Rotation2d abs(Rotation2d in) {
     return in.getRadians() < 0 ? in.unaryMinus() : in;
   }
+
+  public static Rotation2d normalize(Rotation2d in) {
+    return (in.getRadians() <= Math.PI) ? in : new Rotation2d(2 * Math.PI - in.getRadians());
+  }
+
 }
